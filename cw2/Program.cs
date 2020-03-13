fa33a2b540264627143869fa3eea7e8a107aa50c
Sasha Rzepkowska <s18316@pjwstk.edu.pl>
2020-03-13 21:56:17 +0100

    dziala

--- cw2/Program.cs
+++ cw2/Program.cs
@@ -1,68 +1,127 @@
-﻿using System;
+﻿using cw2;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
 
-namespace cw2
+namespace Cw2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            try
-            {
-                var path = @"C:\Users\s18316\Desktop\dane.csv";
-                var lines = File.ReadLines(path);
-                //TODO stworzenie pliku łog.txt do ktorego beda wpisywane bledy
+           
+            var log = File.Create("łog.txt");
 
-                var today = DateTime.Now;
+            StreamWriter writer = new StreamWriter(log);
+            
+            
+            
+            string[] dane = new string[3];
 
-                string tmp;
-                string[] tab;
+            if (args.Length >= 1)
+                dane[0] = args[0];
+            else
+                dane[0] = "data.csv";
 
-               //TODO var hs = new HashSet<Student>(new OwnComparator);
+            if (args.Length >= 2)
+                dane[1] = args[1];
+            else
+                dane[1] = "żesult.xml";
 
+            if (args.Length >= 3)
+                dane[2] = args[2];
+            else
+                dane[2] = "xml";
 
-
-
-                //dateType - konwertowac
-                foreach (var line in lines)
+                try
                 {
-                    tmp = line;
-                    tab = tmp.Split(",");
-                    if (tab.Length == 9)
-                    {
-                        //hs.Add(tab);
 
-                    }
-                    // Console.WriteLine(line);
+                    var listaStud = new HashSet<Student>(new OwnComparator());
+
+
+                    var stream = new StreamReader(File.OpenRead(Path.GetFullPath(dane[0])));
+
+                    string line = stream.ReadLine();
+
+
+                    while (line != null)
+                    {
+
+                        bool wpisywac = true;
+
+                        string[] student = line.Split(',');
+
+                          var regex = new Regex("$\\s+");
+                        for (int i = 0; i < student.Length; i++)
+                        {
+                            var matches = regex.Matches(student[i]);
+
+                            if (matches.Count > 0)
+                            {
+                                writer.WriteLine(line);
+                                wpisywac = false;
+                            }
+                        }
+
+                        if (student.Length == 9 && wpisywac == true)
+                        {
+                            var st = new Student
+                            {
+
+                                fName = student[0],
+                                lName = student[1],
+                                sName = student[2],
+                                sMode = student[3],
+                                index = student[4],
+                                birthdate = student[5],
+                                email = student[6],
+                                mothersName = student[7],
+                                fathersName = student[8]
+
+
+
+                            };
+
+
+                            if (!listaStud.Add(st))
+                            {
+                                writer.WriteLine(line);
+                            }
+
+
+                        }
+                        line = stream.ReadLine();
 
                 }
 
-                //  TODO foreach(Student in )
+                    FileStream XML = new FileStream(dane[1], FileMode.Create);
+                            XmlSerializer serializer = new XmlSerializer(typeof(HashSet<Student>), new XmlRootAttribute("uczelnia"));
+                            serializer.Serialize(XML, listaStud);
 
+                            XML.Close();
+                            XML.Dispose();
+                         
+                        
+                    
 
-
-                /*to co pan napisal
-                 * dos.Add(students);
-                 * var studentInfo = new XElement("student", new XAttribute( new
-                 * new XElement("fname","Jan
-                 * 
-                 * students.add(studInfo);
-                 * 
-                 * doc.Save(@"output.xml");
-                 */
-
-            } catch (ArgumentException e)
-            {
-                Console.WriteLine("Podana ścieżka jest niepoprawna");
-                //TODO wpisywanie do pliku
-            }
-            catch (FileNotFoundException e)
-            {
-                Console.WriteLine("Plik nazwa nie istnieje");
-                //TODO wpisywanie do pliku
-            }
+                }
+                catch (ArgumentException e)
+                {
+                    string mess = "Podana ścieżka jest niepoprawna";
+                    Console.WriteLine(mess);
+                    writer.WriteLine(mess);
+                }
+                catch (FileNotFoundException e)
+                {
+                    string mess = "nazwa Pliku nie istnieje";
+                    Console.WriteLine(mess);
+                    writer.WriteLine(mess);
+                }
+            
+                             writer.Close();
+                            writer.Dispose();
         }
-
     }
 }
--- cw2/Student.cs
+++ cw2/Student.cs
@@ -1,32 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Xml.Serialization;
 
 namespace cw2
 {
-   public class Student
+    [Serializable]
+    public class Student
     {
-        [XmlElement(elementName: "index")]
-        public string index { get; set; }
-        [XmlElement(elementName: "fname")]
-        public string fName { get; set; }
-        [XmlElement(elementName: "lname")]
-        public string lName { get; set; }
-        [XmlElement(elementName: "birthdate")]
-        public string birthdate { get; set; }
-        [XmlElement(elementName: "email")]
-        public string email { get; set; }
+        [XmlElement(elementName: "index")] public string index { get; set; }
+        [XmlElement(elementName: "fname")] public string fName { get; set; }
+        [XmlElement(elementName: "lname")] public string lName { get; set; }
+        [XmlElement(elementName: "birthdate")] public string birthdate { get; set; }
+        [XmlElement(elementName: "email")] public string email { get; set; }
+
         [XmlElement(elementName: "mothersName")]
         public string mothersName { get; set; }
+
         [XmlElement(elementName: "fathersName")]
         public string fathersName { get; set; }
-        [XmlElement(elementName: "studiesName")]
-        public string studiesName{ get; set; }
-        [XmlElement(elementName: "studiesMode")]
-        public string studiesMode { get; set; }
+
+        
+        
+        [XmlElement(elementName: "name")]
+
+        public string sName { get; set; }
+        [XmlElement(elementName: "mode")] 
+        public string sMode { get; set; }
+
+    
 
 
-
-
-    }
+}
 }
