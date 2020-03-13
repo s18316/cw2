cd720abf65fac701cf5a13b7da264e6440e14278
Sasha Rzepkowska <s18316@pjwstk.edu.pl>
2020-03-13 11:31:14 +0100

    popr stud

--- cw2/Student.cs
+++ cw2/Student.cs
@@ -6,15 +6,27 @@ namespace cw2
 {
    public class Student
     {
+        [XmlElement(elementName: "index")]
         public string index { get; set; }
+        [XmlElement(elementName: "fname")]
         public string fName { get; set; }
+        [XmlElement(elementName: "lname")]
         public string lName { get; set; }
+        [XmlElement(elementName: "birthdate")]
         public string birthdate { get; set; }
+        [XmlElement(elementName: "email")]
         public string email { get; set; }
+        [XmlElement(elementName: "mothersName")]
         public string mothersName { get; set; }
+        [XmlElement(elementName: "fathersName")]
         public string fathersName { get; set; }
+        [XmlElement(elementName: "studiesName")]
         public string studiesName{ get; set; }
+        [XmlElement(elementName: "studiesMode")]
         public string studiesMode { get; set; }
+
+
+
 
     }
 }
